cce8c6454729929526173854ae0ed5c7af7463b1
jakobkukla <jakob.kukla@gmail.com>
2025-03-02 21:33:28 +0100

    feat(template/devenv): add devenv-up command

--- devenv/flake.nix
+++ devenv/flake.nix
@@ -15,6 +15,8 @@
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
     in {
+      packages.devenv-up = self.devShells.${system}.default.config.procfileScript;
+
       devShells.default = devenv.lib.mkShell {
         inherit inputs pkgs;
         modules = [
